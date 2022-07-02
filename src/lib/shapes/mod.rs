867267ffbff8039c62f22adbaf9eb12e23048dfd
Josh Read <joshua-read@hotmail.co.uk>
2022-07-02 07:51:35 +0100

    Add Shape trait

---
+++ src/lib/shapes/mod.rs
@@ -0,0 +1,13 @@
+use crate::ray::Ray;
+use nalgebra::Vector3;
+
+pub trait Shape {
+    fn intersection(&self, ray: &Ray) -> Option<f64> {
+        unimplemented!()
+    }
+
+    /// As a convention normal should point outwards
+    fn normal(&self, position: Vector3<f64>) -> Vector3<f64> {
+        unimplemented!()
+    }
+}
