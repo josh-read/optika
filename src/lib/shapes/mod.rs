e5c8855ddb7910aa83eba43b3834df1791704bf7
Josh Read <joshua-read@hotmail.co.uk>
2022-07-02 12:36:32 +0100

    Force reimplementation of intersection and normal method for Shapes

--- src/lib/shapes/mod.rs
+++ src/lib/shapes/mod.rs
@@ -2,14 +2,10 @@ use crate::Ray;
 use nalgebra::Vector3;
 
 pub trait Shape {
-    fn intersection(&self, ray: &Ray) -> Option<f64> {
-        unimplemented!()
-    }
+    fn intersection(&self, ray: &Ray) -> Option<f64>;
 
     /// As a convention normal should point outwards
-    fn normal(&self, position: Vector3<f64>) -> Vector3<f64> {
-        unimplemented!()
-    }
+    fn normal(&self, position: Vector3<f64>) -> Vector3<f64>;
 
     fn centre(&self) -> Vector3<f64> {
         unimplemented!()
