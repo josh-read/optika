de686f69c7e2bac79f7dacf0a80b58bca25bfaad
Josh Read <joshua-read@hotmail.co.uk>
2022-07-03 09:45:46 +0100

    Construct test optical system in main.rs and call trace_ray

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,5 +1,24 @@
 use optika::*;
+use optika::constants::*;
 
 fn main() {
-    
+    let lens_1 = OpticalElement::new_thin_lens(
+        100.0 * FORWARD,
+        BACKWARD,
+        100.0,
+        Some(50.0)
+    );
+    let lens_2 = OpticalElement::new_thin_lens(
+        100.0 * FORWARD,
+        BACKWARD,
+        200.0,
+        Some(50.0)
+    );
+    let elements: Vec<OpticalElement> = vec![lens_1, lens_2];
+    let os = OpticalSystem::new(
+        elements,
+        AXIAL,
+        1e-6
+    );
+    os.trace_ray();
 }
